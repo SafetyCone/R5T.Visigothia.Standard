4b6e0d3f77a2689add52aba207459fa0f803e158
MinexAutomation <minex.automation@gmail.com>
2020-01-20 13:16:10 -0800

    Added IServiceCollection extensions.

--- source/R5T.Visigothia.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Visigothia.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Visigothia.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Visigothia.Local.Standard;
+
+
+namespace R5T.Visigothia.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the standard <see cref="IUserProfileDirectoryPathProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddUserProfileDirectoryPathProvider(this IServiceCollection services)
+        {
+            services.AddLocalUserProfileDirectoryProvider();
+
+            return services;
+        }
+    }
+}
